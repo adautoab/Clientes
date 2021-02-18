3102c8f1ce5c0f2e52197140b8dea9fad536f3d7
User <User@DESKTOP-UJDC6JO>
2021-02-17 22:14:42 -0300

    Primeiro commit

---
+++ src/main/java/Models/Clientes.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author User
+ */
+public class Clientes {
+    private int Id;
+    private String Nome;
+
+    public Clientes(int Id, String Nome) {
+        this.Id = Id;
+        this.Nome = Nome;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+    
+    
+}
